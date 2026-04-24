6a1a1a2da960237300041b276426086bf72e052b
Lynliash <francesco.alves1408@gmail.com>
2026-04-24 15:31:52 +0200

    d

--- src/main/java/francescoalves/finalproject/security/SecurityConfig.java
+++ src/main/java/francescoalves/finalproject/security/SecurityConfig.java
@@ -7,8 +7,6 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -28,11 +26,6 @@ public class SecurityConfig {
 
         httpSecurity.authorizeHttpRequests(http -> http.requestMatchers("/**").permitAll());
         return httpSecurity.build();
-    }
-
-    @Bean
-    public PasswordEncoder getBCrypt() {
-        return new BCryptPasswordEncoder(12);
     }
 }
 
